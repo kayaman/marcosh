6235c5721c2ed79d4290257dcd0b04cd64b777d1
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-07-12 17:33:17 -0300

    init, layout and styles

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import * as config from "$lib/config";
+</script>
+
+<nav>
+  <a href="/" class="title">
+    <b>{config.title}</b>
+  </a>
+
+  <ul class="links">
+    <li>
+      <a href="/about">About</a>
+    </li>
+    <li>
+      <a href="/contact">Contact</a>
+    </li>
+    <li>
+      <a href="/rss" target="_blank">RSS</a>
+    </li>
+  </ul>
+
+  <button>Toggle</button>
+</nav>
+
+<style>
+  nav {
+    padding-block: var(--size-7);
+  }
+
+  .links {
+    margin-block: var(--size-7);
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  @media (min-width: 768px) {
+    nav {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .links {
+      display: flex;
+      gap: var(--size-7);
+      margin-block: 0;
+    }
+  }
+
+</style>
---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'ma.rco.sh.'
+export const description = 'The years go fast but the days go so slow..'
+export const url = dev ? 'http://localhost:3000' : 'https://ma.rco.sh'
