a50e4860315abcc7588641d6ebc5f8734bb0da65
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-07-12 22:22:12 -0300

    displaying posts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,19 +1,19 @@
 export type Category =
-	| 'svelte'
-	| 'sveltekit'
-	| 'javascript'
-	| 'typescript'
-	| 'css'
-	| 'html'
-	| 'other'
-	| 'all'
-	| 'none'
+  | 'svelte'
+  | 'sveltekit'
+  | 'javascript'
+  | 'typescript'
+  | 'css'
+  | 'html'
+  | 'other'
+  | 'all'
+  | 'none'
 
 export type Post = {
-	title: string
-	slug: string
-	description: string
-	date: string
-	categories: Category[]
-	published: boolean
+  title: string
+  slug: string
+  description: string
+  date: string
+  categories: Category[]
+  published: boolean
 }
