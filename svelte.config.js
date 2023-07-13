2445b2e1ffd50f1d0b3762c54e2ecf8e6e99eee4
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-07-13 05:02:01 -0300

    syntax highlighting

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
-import { mdsvex } from 'mdsvex'
+import { escapeSvelte, mdsvex } from 'mdsvex'
+import shiki from 'shiki'
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexConfig = {
   extensions: ['.md'],
+  highlight: {
+    highlighter: async (code, lang = 'text') => {
+      const highlighter = await shiki.getHighlighter({ theme: 'poimandres' })
+      const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
+      return `{@html \`${html}\`}`
+    },
+  },
 }
 
 /** @type {import('@sveltejs/kit').Config} */
