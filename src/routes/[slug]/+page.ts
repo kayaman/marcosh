beb4d81b3e46d713f22ffbb37b6f1534208ef2ed
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-07-12 23:30:55 -0300

    displaying a post

---
+++ src/routes/[slug]/+page.ts
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit'
+
+export const load = async ({ params }) => {
+  try {
+    const post = await import(`../../posts/${params.slug}.md`)
+    return {
+      content: post.default,
+      meta: post.metadata,
+    }
+  } catch (e) {
+    throw error(404, `Post not found ${params.slug}`)
+  }
+}
