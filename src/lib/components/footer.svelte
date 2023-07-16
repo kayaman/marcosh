df8e6361197d5f6d2156217ecc7be09682f47a92
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-07-16 11:50:00 -0300

    aria labels

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
   import * as config from '$lib/config'
   import { theme } from '$lib/stores'
-  import { GithubIcon, LinkedinIcon, TwitterIcon, InstagramIcon, Code2Icon } from 'lucide-svelte'
+  import { GithubIcon, LinkedinIcon, TwitterIcon, InstagramIcon, Code2Icon, FileTextIcon } from 'lucide-svelte'
   import BookmarcoDM from './Bookmarco_dark_mode.svelte';
   import BookmarcoLM from './Bookmarco_light_mode.svelte';
 
   let selectedTheme = BookmarcoDM
 
   theme.subscribe(value => {
-    console.log(value)
     if (value === 'dark') {
       selectedTheme = BookmarcoDM
     } else {
@@ -22,12 +21,13 @@
   <p><span>{config.bottomline}</span></p>
   <p>{config.title} &copy; {new Date().getFullYear()}</p>
   <p>
-    <a href="https://github.com/kayaman" target="_blank"><GithubIcon display="inline" size="20"/></a>
-    <a href="https://github.com/kayaman/marcosh" target="_blank"><Code2Icon display="inline" size="20"/></a>
-    <a href="https://bookma.rco.sh" target="_blank"><svelte:component this={selectedTheme} /></a>
-    <a href="https://linkedin.com/in/marcoantoniogonzalezjunior" target="_blank"><LinkedinIcon display="inline" size="20"/></a>
-    <a href="https://twitter.com/kayaman" target="_blank"><TwitterIcon display="inline" size="20"/></a>
-    <a href="https://instagram.com/marcoantoniogonzalezjunior" target="_blank"><InstagramIcon display="inline" size="20"/></a>
+    <a href="https://github.com/kayaman" target="_blank"><GithubIcon aria-label="GitHub" display="inline" size="20"/></a>
+    <a href="https://github.com/kayaman/marcosh" target="_blank"><Code2Icon aria-label="Blog" display="inline" size="20"/></a>
+    <a href="https://bookma.rco.sh" target="_blank" aria-label="Blogmarks"><svelte:component this={selectedTheme} /></a>
+    <a href="https://linkedin.com/in/marcoantoniogonzalezjunior" target="_blank"><LinkedinIcon display="inline" size="20" aria-label="LinkedIn" /></a>
+    <a href="https://github.com/kayaman/resume/blob/main/MarcoAntonioGonzalezJunior_en.pdf" target="_blank"><FileTextIcon aria-label="Résumé" display="inline" size="20"/></a>
+    <a href="https://twitter.com/kayaman" target="_blank"><TwitterIcon aria-label="Twitter" display="inline" size="20"/></a>
+    <a href="https://instagram.com/marcoantoniogonzalezjunior" target="_blank"><InstagramIcon aria-label="Instagram" display="inline" size="20" /></a>
 
   </p>
 </footer>
---
+++ src/posts/2023-07-01-svelte-course.md
@@ -0,0 +1,40 @@
+---
+title: Svelte Course
+description: Svelte Course Study Notes
+date: '2023-07-01'
+categories:
+  - svelte
+  - sveltekit
+  - learning
+  - study-notes
+published: false
+---
+
+## 1. Introduction
+
+Whats is Svelte?
+
+## 2. The Svelte Compiler
+
+Compare the payloads of React and Svelte.
+
+```javascript
+import './App.css'
+
+export default function App() {
+  return <h1>🔥 React</h1>
+}
+```
+
+```svelte
+<script>
+  import './styles.css'
+</script>
+
+<h1>🔥 Svelte</h1>
+```
+
+## References
+
+- [Svelte](https://svelte.dev/)
+- [SvelteKit Course](https://www.youtube.com/watch?v=BrkrOjknC_E&list=PLA9WiRZ-IS_ylnMYxIFCsZN6xVVSvLuHk&index=1)
