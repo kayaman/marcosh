fb2ace2d2f9f7745934ed1ba98cca31dc1a5b9e1
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-07-21 09:03:49 -0300

    discord

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
   import * as config from '$lib/config'
   import { theme } from '$lib/stores'
-  import { GithubIcon, LinkedinIcon, TwitterIcon, InstagramIcon, Code2Icon, FileTextIcon } from 'lucide-svelte'
+  import { Github, Linkedin, Twitter, Instagram, Code2, FileText } from 'lucide-svelte'
   import BookmarcoDM from './Bookmarco_dark_mode.svelte';
   import BookmarcoLM from './Bookmarco_light_mode.svelte';
+  import DiscordDM from './Discord_dark_mode.svelte';
+  import DiscordLM from './Discord_light_mode.svelte';
 
-  let selectedTheme = BookmarcoDM
+  let selectedBookmarcoTheme = BookmarcoDM
+  let selectedDiscordTheme = DiscordDM
 
   theme.subscribe(value => {
     if (value === 'dark' || value === 'dim') {
-      selectedTheme = BookmarcoDM
+      selectedBookmarcoTheme = BookmarcoDM
+      selectedDiscordTheme = DiscordDM
     } else {
-      selectedTheme = BookmarcoLM
+      selectedBookmarcoTheme = BookmarcoLM
+      selectedDiscordTheme = DiscordLM
     }
   })
 </script>
@@ -21,13 +26,14 @@
   <p>{config.bottomline}</p>
   <p>{config.title} &copy; {new Date().getFullYear()}</p>
   <p>
-    <a href="https://github.com/kayaman" target="_blank"><GithubIcon aria-label="GitHub" display="inline" size="22"/></a>
-    <a href="https://github.com/kayaman/marcosh" target="_blank"><Code2Icon aria-label="Blog" display="inline" size="24"/></a>
-    <a href="https://bookma.rco.sh" target="_blank" aria-label="Blogmarks"><svelte:component this={selectedTheme} /></a>
-    <a href="https://linkedin.com/in/marcoantoniogonzalezjunior" target="_blank"><LinkedinIcon display="inline" size="24" aria-label="LinkedIn" /></a>
-    <a href="https://github.com/kayaman/resume/blob/main/MarcoAntonioGonzalezJunior_en.pdf" target="_blank"><FileTextIcon aria-label="Résumé" display="inline" size="24"/></a>
-    <a href="https://twitter.com/kayaman" target="_blank"><TwitterIcon aria-label="Twitter" display="inline" size="24"/></a>
-    <a href="https://instagram.com/marcoantoniogonzalezjunior" target="_blank"><InstagramIcon aria-label="Instagram" display="inline" size="24" /></a>
+    <a href="https://github.com/kayaman" target="_blank"><Github aria-label="GitHub" display="inline" size="22"/></a>
+    <a href="https://github.com/kayaman/marcosh" target="_blank"><Code2 aria-label="Blog" display="inline" size="24"/></a>
+    <a href="https://bookma.rco.sh" target="_blank" aria-label="Blogmarks"><svelte:component this={selectedBookmarcoTheme} /></a>
+    <a href="https://linkedin.com/in/marcoantoniogonzalezjunior" target="_blank"><Linkedin display="inline" size="24" aria-label="LinkedIn" /></a>
+    <a href="https://github.com/kayaman/resume/blob/main/MarcoAntonioGonzalezJunior_en.pdf" target="_blank"><FileText aria-label="Résumé" display="inline" size="24"/></a>
+    <a href="https://discord.gg/5PQYtGN9" target="_blank" aria-label="Discord"><svelte:component this={selectedDiscordTheme} /></a>
+    <a href="https://twitter.com/kayaman" target="_blank"><Twitter aria-label="Twitter" display="inline" size="24"/></a>
+    <a href="https://instagram.com/marcoantoniogonzalezjunior" target="_blank"><Instagram aria-label="Instagram" display="inline" size="24" /></a>
   </p>
 </footer>
 
